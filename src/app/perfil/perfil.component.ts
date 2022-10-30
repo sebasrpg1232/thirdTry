d903e6fe9c63039f7984a0487d5d641ccc9fad71
sebasrpg1232 <sebasrpg1232@gmail.com>
2022-10-30 14:50:51 -0500

    sebas

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
   constructor() {}
-
+  nombre = "";
+  documento = "";
+  correo = "";
+  direccion = "";
+  telefono = "";
+  
+  sendValues(){
+  this.nombre = "Carlos Enrique Guillent Carruyo";
+  this.documento = "757878";
+  this.correo = "carlosguillent@algo.com";
+  this.direccion = "Maracaibo-Haticos, Calle 1000b #35y-44 ";
+  this.telefono = "3224599592";
+  console.log(this.nombre);
+  
+  }
   ngOnInit(): void {}
 }
