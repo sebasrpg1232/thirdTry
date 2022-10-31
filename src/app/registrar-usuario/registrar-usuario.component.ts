dd9fdd7500eca6c4a94d94f740343c437e0b5212
LAPTOP <sebasrpg1232@gmail.com>
2022-10-31 08:32:00 -0500

    Redireccion desde la pagina, ready

--- src/app/registrar-usuario/registrar-usuario.component.ts
+++ src/app/registrar-usuario/registrar-usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registrar-usuario',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrar-usuario.component.css'],
 })
 export class RegistrarUsuarioComponent implements OnInit {
-  constructor() {}
+  constructor(private router:Router) {}
+
+  goToIndex(){
+    this.router.navigate(['index']);
+  }
 
   ngOnInit(): void {}
 }
